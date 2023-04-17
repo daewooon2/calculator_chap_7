74185daf67ca636c5d4881599a057954979d0e58
D416-23\user <a41224416@gmail.com>
2023-04-17 15:01:30 +0900

    modify pow function to check base

--- ctrl.py
+++ ctrl.py
@@ -38,5 +38,12 @@ class Control:
         return a/b
     
     def pow(self, a, b):
+<<<<<<< HEAD
         return pow(a, b)
     
+=======
+        try:
+           if (a==0):
+                raise Exception("Base Error")
+        return pow(a, b)
+>>>>>>> aa5c67c (modify pow function to check base)
